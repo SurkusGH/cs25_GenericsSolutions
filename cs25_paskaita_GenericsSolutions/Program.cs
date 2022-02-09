0280bbea4fb9310fd9da27e6d776774ab723a52c
Surkus <v.surkus@me.com>
2022-02-09 18:09:56 +0200

    Initial commit

---
+++ cs25_paskaita_GenericsSolutions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace cs25_paskaita_GenericsSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cs25_PASKAITA_GenericsSolutions!");
+        }
+
+        //Generics parametrų galima naudoti daugiau negu vieną pvz Method<T1, T2>
+        //Sukurkite klasę kuri priima du generic tipo parametrus.
+        //Klasė turės du kintamuosius kurie atitiks tuos generic parametrus.
+        //Turės keturias funkcijas, dvi iš jų bus atspausdinti T1 ir T2 properties
+        //Kitos dvi funkcijos leis pakeisti reikšmę T1 ir T2 properties.
+    }
+}
+
