d6d73f448a409a7ef7daa56f82bb15d325e212cd
SurkusGH <v.surkus@me.com>
2022-02-09 21:03:15 +0200

    Cosmetics

--- cs25_paskaita_GenericsSolutions/Program.cs
+++ cs25_paskaita_GenericsSolutions/Program.cs
@@ -21,6 +21,7 @@ namespace cs25_paskaita_GenericsSolutions
 
         public static void Solution_01()
         {
+            Console.WriteLine("Solution_01");
             var solution = new Solution_01<string, decimal>(); // <-- jeigu neturėčiau tuščio ctor, čia turėčiau reikšmes nurodyt.
             solution.PrintTandF("Labas", 5m);                   //     Neskamba taisyklingai, nei be empty ctor, nei su juo. IDK. Google nepadeda. Bet veikia.
 
@@ -34,6 +35,7 @@ namespace cs25_paskaita_GenericsSolutions
 
         public static void Solution_02()
         {
+            Console.WriteLine("Solution_02");
             var solution2 = new Solution_02("Stačiakampis", 10f, 5f);
             Console.WriteLine(solution2); // <-- Šitą atliaka tiesiog overridinimas ToString();
             // Tada susikuriu generatoriuką, kuriam paduosiu stringą arg:
@@ -42,6 +44,7 @@ namespace cs25_paskaita_GenericsSolutions
         }
         public static void Solution_03()
         {
+            Console.WriteLine("Solution_03");
             var solution3a = new Solution_03<int>(10, 5);
             solution3a.ValueAddition();
             var solution3b = new Solution_03<float>(10f, 5f);
