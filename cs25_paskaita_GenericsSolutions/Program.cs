fdca128153d0769eece568e891404d8db16929b3
SurkusGH <v.surkus@me.com>
2022-02-09 19:13:24 +0200

    Solution_1 complete

--- cs25_paskaita_GenericsSolutions/Program.cs
+++ cs25_paskaita_GenericsSolutions/Program.cs
@@ -7,13 +7,18 @@ namespace cs25_paskaita_GenericsSolutions
         static void Main(string[] args)
         {
             Console.WriteLine("cs25_PASKAITA_GenericsSolutions!");
-        }
 
-        //Generics parametrų galima naudoti daugiau negu vieną pvz Method<T1, T2>
-        //Sukurkite klasę kuri priima du generic tipo parametrus.
-        //Klasė turės du kintamuosius kurie atitiks tuos generic parametrus.
-        //Turės keturias funkcijas, dvi iš jų bus atspausdinti T1 ir T2 properties
-        //Kitos dvi funkcijos leis pakeisti reikšmę T1 ir T2 properties.
+            var solution = new Solution_1<string , decimal>();
+            solution.PrintTandF("Labas", 5m);
+
+            solution.SetValueT("Viso gero");
+            solution.SetValueF(10m);
+
+            Console.WriteLine();
+
+            solution.PrintTandF();
+
+        }
     }
 }
 
---
+++ cs25_paskaita_GenericsSolutions/Solution_1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs25_paskaita_GenericsSolutions
+{
+    public class Solution_1<T, F>
+    {
+        // Generics parametrų galima naudoti daugiau negu vieną pvz Method<T1, T2>
+        // Sukurkite klasę kuri priima du generic tipo parametrus.
+        // Klasė turės du kintamuosius kurie atitiks tuos generic parametrus.
+
+        public T ValueT { get; set; }
+        public F ValueF { get; set; }
+
+        public Solution_1(T valueT, F valueF)
+        {
+            ValueT = valueT;
+            ValueF = valueF;
+        }
+
+        public Solution_1()
+        {
+        }
+
+        // Turės keturias funkcijas, dvi iš jų bus atspausdinti T1 ir T2 properties
+
+        //public void PrintTandF(T input1, F input2)
+        //{
+        //    Console.WriteLine($"Pirmasis kintamasis: {input1}, jo tipas yra {input1.GetType()} tipo");
+        //    Console.WriteLine($"Antrasis kintamasis: {input2}, jo tipas yra {input2.GetType()} tipo");
+        //}
+        public void PrintTandF(T input1, F input2)
+        {
+            Console.WriteLine($"Pirmasis kintamasis: {input1}, jis yra {input1.GetType()} tipo");
+            Console.WriteLine($"Antrasis kintamasis: {input2}, jis yra {input2.GetType()} tipo");
+        }
+        public void PrintTandF()
+        {
+            Console.WriteLine($"Pirmasis kintamasis: {ValueT}, jis yra {ValueT.GetType()} tipo");
+            Console.WriteLine($"Antrasis kintamasis: {ValueF}, jis yra {ValueF.GetType()} tipo");
+        }
+
+        // Kitos dvi funkcijos leis pakeisti reikšmę T1 ir T2 properties.
+
+        public T SetValueT(T input)
+        {
+            ValueT = input;
+            Console.WriteLine($"Naujas T value yra {ValueT}");
+            return ValueT;
+
+        }
+
+        public F SetValueF(F input)
+        {
+            ValueF = input;
+            Console.WriteLine($"Naujas F value yra {ValueF}");
+            return ValueF;
+
+        }
+    }
+}
